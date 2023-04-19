9e7c9d6afe12ab1ce902e60c5a9c78fe2e677029
firmafadi <firmafirmansyah@gmail.com>
2023-04-19 21:55:40 +0700

    create login, session and logout

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { User } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-const Navbar = () => {
+interface NavbarProps{
+    currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+    currentUser
+}) => {
     return (
         <div className="fixed w-full bg-white z-10 shadow-sw">
             <div className="py-4 border-b-[1px]">
@@ -20,7 +27,7 @@ const Navbar = () => {
                     ">
                         <Logo/> 
                         <Search/>
-                        <UserMenu/>
+                        <UserMenu currentUser={currentUser} />
                     </div>
                 </Container>
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: 'AirCnc',
@@ -19,11 +20,14 @@ const font = Nunito(
   }
 );
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  
+  const currentUser = await getCurrentUser();
+  
   return (
     <html lang="en">
       <body className={font.className}>
@@ -32,7 +36,7 @@ export default function RootLayout({
         <ToasterProvider/>
         <RegisterModal/>
         <LoginModal/>
-        <Navbar/>
+        <Navbar currentUser={currentUser}/>
       </ClientOnly>
         {children}
         </body>
